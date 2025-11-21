fa1da51efca9a252acccbbf0a72a4210a406c808
Akshatha Vernekar <akshathavernekar159@gmail.com>
2025-11-21 23:53:26 +0530

    pageObjectCommit

---
+++ pageObjects/loginPage.js
@@ -0,0 +1,21 @@
+class LoginPage{
+    constructor(page)
+    {
+        this.page=page;
+        this.signInButton=page.locator("[value='Login']");
+        this.username=page.locator('#userEmail');
+        this.password=page.locator('#userPassword');
+
+    }
+
+     async goTo(){
+       await  this.page.goto("https://rahulshettyacademy.com/client");
+    }
+    async validLogin(email,passsword)
+    {
+        await  this.username.fill(email);
+        await this.password.fill(passsword);
+        await  this.signInButton.click();
+    }
+}
+module.exports={LoginPage};
